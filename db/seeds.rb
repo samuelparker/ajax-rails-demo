2efe85e319522adb14b3f2cb5e61ffa6bb4d67a9
Sam Parker <samuel.gage.parker@gmail.com>
2016-10-06 21:16:13 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
 Task.delete_all
 
-Task.create!(description: 'This is a task description',
-             deadline: '2014-07-01 01:00:00')
+Task.create!(description: 'This is a task description')
 
-Task.create!(description: 'This is another task description',
-             deadline: '2014-07-02 01:00:00')
+Task.create!(description: 'This is another task description')
 
-Task.create!(description: 'Yet another task description',
-             deadline: '2014-07-03 01:00:00')
+Task.create!(description: 'Yet another task description')
